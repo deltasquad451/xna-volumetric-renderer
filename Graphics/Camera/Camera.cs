c28cb974d2c64b3b62c3e86f264f90c257d6c662
jorgegarridor <jorgegarridor@28ad339c-c74e-11de-9d4f-f947ee5921c8>
2009-11-27 21:07:09 +0000

    Added zooming (Up and Down Arrows) Rotation gives unexpected results (left and right), still investigating.  Any ideas on this would be appreciated. You can reset view with the Spacebar

--- Graphics/Camera/Camera.cs
+++ Graphics/Camera/Camera.cs
@@ -19,11 +19,19 @@ namespace Renderer.Graphics.Camera
         public Matrix projectionMat { get; private set; }
         public float fov { get; private set; }
         public float aspectRatio { get; private set; }
+        public int keyflag = 0;
+        public float camx = 2.5f;
+        public float camy = 2.5f;
+        public float camz = -7.0f;
+
+        private KeyboardState currentKeyboardState;
+
         #endregion
 
         #region Initialization
         public Camera(Viewport viewport)
         {
+
             aspectRatio = (float)viewport.Width / (float)viewport.Height;
             fov = MathHelper.ToRadians(60.0f);
             projectionMat = Matrix.CreatePerspectiveFieldOfView(fov, aspectRatio,
@@ -36,7 +44,62 @@ namespace Renderer.Graphics.Camera
         #region Methods
         public void Update()
         {
-            viewMat = Matrix.CreateLookAt(position, target, Vector3.Up);
+            currentKeyboardState = Keyboard.GetState();
+
+            if (keyflag == 0)
+            {
+                viewMat = Matrix.CreateLookAt(position, target, Vector3.Up);
+            }
+            if (currentKeyboardState.IsKeyDown(Keys.Left)){
+                camx -= 0.5f;
+                camz += 0.5f;
+                //if (camx > -3.0f) { camx = -3.0f; }
+                if (camx < -30.0f) { camx = -30.0f; }
+                if (camz > -3.0f) { camz = -3.0f; }
+                if (camz < -30.0f) { camz = -30.0f; }
+                position = new Vector3(camx, camy, camz);
+                //target = new Vector3(camx, 2.5f, 2.5f);
+                viewMat = Matrix.CreateLookAt(position, target, Vector3.Up);
+                keyflag = 1;
+            }
+            else if (currentKeyboardState.IsKeyDown(Keys.Right)){
+                camx += 0.5f;
+                camz -= 0.5f;
+                //if (camx > -3.0f) { camx = -3.0f; }
+                if (camx < -30.0f) { camx = -30.0f; }
+                if (camz > -3.0f) { camz = -3.0f; }
+                if (camz < -30.0f) { camz = -30.0f; }
+                position = new Vector3(camx, camy, camz);
+                //target = new Vector3(camx, 2.5f, 2.5f);
+                viewMat = Matrix.CreateLookAt(position, target, Vector3.Up);
+                keyflag = 1;
+            }
+            else if (currentKeyboardState.IsKeyDown(Keys.Up))
+            {
+                camz += 0.5f;
+                if (camz > -3.0f) { camz = -3.0f; }
+                if (camz < -30.0f){camz = -30.0f;}
+                position = new Vector3(camx, camy, camz); 
+                viewMat = Matrix.CreateLookAt(position, target, Vector3.Up);
+                keyflag = 1;
+            }
+            else if (currentKeyboardState.IsKeyDown(Keys.Down))
+            {
+                camz -= 0.5f;
+                if (camz > -3.0f){camz = -3.0f;}
+                if (camz < -30.0f){camz = -30.0f;}
+                position = new Vector3(camx, camy, camz); 
+                viewMat = Matrix.CreateLookAt(position, target, Vector3.Up);
+                keyflag = 1;
+            }
+            else if (currentKeyboardState.IsKeyDown(Keys.Space))
+            {
+                camx = 2.5f; camy = 2.5f; camz = -7.0f;
+                position = new Vector3(camx, camy, camz);
+                target = new Vector3(camx, camy, 2.5f); 
+                viewMat = Matrix.CreateLookAt(position, target, Vector3.Up);
+                keyflag = 1;
+            }
         }
         #endregion
     }
