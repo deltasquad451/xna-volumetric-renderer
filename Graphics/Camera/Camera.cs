bea0a9690ddd8b50c0dc06201a8d6f299cc899b6
darkspy5 <darkspy5@28ad339c-c74e-11de-9d4f-f947ee5921c8>
2009-11-29 23:14:22 +0000

    Fixed bug causing the volume to rotate the opposite direction as the camera.  Apparently, the XNA CreateLookAt() and CreatePerspectiveFOV() functions don't create the standard matrices one would expect, so now we setup these matrices manually.

--- Graphics/Camera/Camera.cs
+++ Graphics/Camera/Camera.cs
@@ -23,6 +23,7 @@ namespace Renderer.Graphics.Camera
         public Matrix projectionMat { get; private set; }
         public float fov { get; private set; }
         public float aspectRatio { get; private set; }
+        public Quaternion rotQuat { get; private set; }
 
         private KeyboardState currentKeyboardState;
 
@@ -34,10 +35,25 @@ namespace Renderer.Graphics.Camera
 
             aspectRatio = (float)viewport.Width / (float)viewport.Height;
             fov = MathHelper.ToRadians(60.0f);
-            projectionMat = Matrix.CreatePerspectiveFieldOfView(fov, aspectRatio,
-                                                    NEAR_PLANE_DIST, FAR_PLANE_DIST);
+
+            //projectionMat = Matrix.CreatePerspectiveFieldOfView(fov, aspectRatio,
+            //                                        NEAR_PLANE_DIST, FAR_PLANE_DIST);
+
+            Matrix proj;
+            proj.M11 = 1 / ((float)Math.Tan(fov / 2.0f) * aspectRatio);
+            proj.M12 = proj.M13 = proj.M14 = 0.0f;
+            proj.M21 = proj.M23 = proj.M24 = 0.0f;
+            proj.M22 = 1 / ((float)Math.Tan(fov / 2.0f));
+            proj.M31 = proj.M32 = 0.0f;
+            proj.M33 = FAR_PLANE_DIST / (FAR_PLANE_DIST - NEAR_PLANE_DIST);
+            proj.M34 = 1.0f;
+            proj.M41 = proj.M42 = proj.M44 = 0.0f;
+            proj.M43 = -NEAR_PLANE_DIST * FAR_PLANE_DIST / (FAR_PLANE_DIST - NEAR_PLANE_DIST);
+
+            projectionMat = proj;
             position = new Vector3(2.5f, 2.5f, -7.0f);
             target = new Vector3(2.5f, 2.5f, 2.5f);
+            rotQuat = Quaternion.CreateFromYawPitchRoll(0, 0, 0);
         }
         #endregion
 
@@ -52,6 +68,7 @@ namespace Renderer.Graphics.Camera
                 Vector4 targetToPos = new Vector4(position - target, 0);
                 targetToPos = Vector4.Transform(targetToPos, rotationY);
                 position = new Vector3(target.X + targetToPos.X, target.Y + targetToPos.Y, target.Z + targetToPos.Z);
+                rotQuat = Quaternion.Normalize(Quaternion.CreateFromRotationMatrix(rotationY) * rotQuat);
             }
             else if (currentKeyboardState.IsKeyDown(Keys.Right)){
                 Matrix rotationY;
@@ -59,6 +76,7 @@ namespace Renderer.Graphics.Camera
                 Vector4 targetToPos = new Vector4(position - target, 0);
                 targetToPos = Vector4.Transform(targetToPos, rotationY);
                 position = new Vector3(target.X + targetToPos.X, target.Y + targetToPos.Y, target.Z + targetToPos.Z);
+                rotQuat = Quaternion.Normalize(Quaternion.CreateFromRotationMatrix(rotationY) * rotQuat);
             }
             else if (currentKeyboardState.IsKeyDown(Keys.Up))
             {
@@ -85,10 +103,15 @@ namespace Renderer.Graphics.Camera
             else if (currentKeyboardState.IsKeyDown(Keys.Space))
             {
                 position = new Vector3(2.5f, 2.5f, -7.0f);
-                target = new Vector3(2.5f, 2.5f, 2.5f);  
+                target = new Vector3(2.5f, 2.5f, 2.5f);
+                rotQuat = Quaternion.CreateFromYawPitchRoll(0, 0, 0);
             }
 
-            viewMat = Matrix.CreateLookAt(position, target, Vector3.Up);
+            //viewMat = Matrix.CreateLookAt(position, target, Vector3.Up);
+            viewMat = Matrix.Invert(
+                        Matrix.CreateFromQuaternion(rotQuat) *
+                        Matrix.CreateTranslation(position)
+                      );
         }
         #endregion
     }
